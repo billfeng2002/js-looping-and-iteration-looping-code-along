84aa11015e601a1b466c28f4d45c2afe4c4a2c64
Bill <billfeng2002@gmail.com>
2021-07-19 20:53:47 -0700

    commit tree 6435934fee54d31ea899de71ba1c2a2c01e44c53

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function countDown(start){
+    while(start>=0){
+        console.log(start)
+        start--;
+    }
+}
+
+function writeCards(names, type){
+    let messages=[];
+    for(let i=0; i< names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`)
+    }
+    return messages;
+}
